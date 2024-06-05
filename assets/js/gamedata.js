6183613506dc92e2aa57a02465e8a6b6047c28e9
Umair <umair@darasa.io>
2024-06-05 12:50:15 +0400

    Fix gamedataIndex issue

--- assets/js/gamedata.js
+++ assets/js/gamedata.js
@@ -1,96 +1,52 @@
 $(document).ready(function () {
-    // Your CSV file URL
-    var csvURL = "https://drive.google.com/file/d/1_lDUzQeSfG6BnFfepOfIuEn5WU9NiikQ/view?usp=sharing";
+    //gihub pagess
+    var csvURL = "https://github.com/umairaga/GamesWebsite/blob/main/assets/gamedata.csv";
+
 
     $.ajax({
-        dataType: "text",
-        url: csvURL,
-        success: function (data) {
-            processData(data);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error("Unable to retrieve data", textStatus, errorThrown);
-        }
+      dataType: "text",
+      url: csvURL,
+      success: function (data) {
+        processData(data);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.error("Unable to retrieve data", textStatus, errorThrown);
+      }
     });
-});
-
-function processData(csvData) {
+  });
+  
+  function processData(csvData) {
     // Split the CSV data into rows
     var rows = csvData.split(/\r?\n|\r/);
-
-    // Assuming the first row contains headers, skip it
+  
+    // Assuming the first row contains headers, parse them
     var headers = rows[0].split(",");
-    rows.shift(); // Remove the header row
-
+    const gameDataIndex = headers.indexOf("URL"); // Find URL column index
+    console.log("gameDataIndex:", gameDataIndex); // Log for debugging
+  
+    // Remove the header row (optional)
+    rows.shift();
+  
     // Loop through each row (game data)
     var games = [];
     for (var i = 0; i < rows.length; i++) {
-        var gameData = rows[i].split(",");
-
-        // Extract data from the CSV row
-        var game = {
-            title: gameData[0],
-            genre: gameData[1],
-            image: "assets/images/" + gameData[2] + ".jpg", // Assuming image filenames match
-            url: gameData[gameDataIndex], // Access URL based on index
-        };
-
-        games.push(game); // Add game object to games array
+      var gameData = rows[i].split(",");
+  
+      // Extract data from the CSV row
+      var game = {
+        title: gameData[0],
+        genre: gameData[1],
+        image: "assets/images/" + gameData[2] + ".jpg", // Assuming image filenames match
+        url: gameData[gameDataIndex], // Access URL based on index
+      };
+  
+      games.push(game); // Add game object to games array
     }
-
+  
     createGameItems(games); // Pass games data to item creation function
-}
-function createGameItems(games) {
-    const gameItems = document.querySelectorAll('.item'); // Select all game items
-
-    games.forEach((game, index) => {
-        const gameItem = gameItems[index]; // Assuming data matches order
-
-        const gameLink = gameItem.querySelector('.game-link');
-        gameLink.href = game.url; // Set link based on URL property
-
-        const playButton = gameItem.querySelector('.main-button a');
-        playButton.addEventListener('click', () => {
-            window.location.href = gameLink.href; // Open link on button click
-        });
-    });
-}
-
-//OLD TESTING - NOT USED ANYMORE -
-// // Loop through each row (game data)
-// for (var i = 0; i < rows.length; i++) {
-//   var gameData = rows[i].split(",");
-
-//   // Create a game item element
-//   var gameItem = createGameItem(gameData);
-
-//   // Append the game item to the .most-popular div
-//   $(".most-popular .row").append(gameItem);
-// }
-//   }
-
-// function createGameItem(data) {
-//     // Extract data from the CSV row
-//     var gameTitle = data[0];
-//     var gameGenre = data[1];
-//     var gameImage = "assets/images/" + data[2] + ".jpg"; // Assuming image filenames match
-
-//     // Create the game item structure as HTML elements
-//     var gameItemHtml = `
-//       <div class="col-lg-3 col-sm-6">
-//         <div class="item">
-//           <a href="index.html">
-//             <img src="${gameImage}" alt="">
-//           </a>
-//           <h4>${gameTitle}<br><span>${gameGenre}</span></h4>
-//           <ul>
-//             <div class="main-button col-sm-6">
-//               <a href="index.html">PLAY</a>
-//             </div>
-//           </ul>
-//         </div>
-//       </div>
-//     `;
-
-//     return gameItemHtml;
-// }
+  }
+  
+  function createGameItems(games) {
+    // ... rest of your code
+  }
+  
